664d16673a68030d66ee491a7dc2aa420d6363ad
AristideExe <aristidepr@gmail.com>
2026-02-02 18:12:46 +0100

    feat: improve error 404 page

--- src/components/Error404/Error404.jsx
+++ src/components/Error404/Error404.jsx
@@ -1,7 +1,15 @@
+import {Link} from "react-router";
+import {homePath} from "../../routes.jsx";
+
 const Error404 = () => (
-    <>
-        <h1>Error 404</h1>
-    </>
+    <div className="flex-1 flex justify-center items-center">
+        <div className="px-4">
+            <h1 className="text-2xl">Erreur 404</h1>
+            <p>Nous ne savons pas trop comment vous avez atteri sur cette page, cliquez
+                <Link className="text-blue-500 underline" to={homePath}> ici pour revenir à l'accueil</Link>
+            </p>
+        </div>
+    </div>
 );
 
 Error404.propTypes = {};
