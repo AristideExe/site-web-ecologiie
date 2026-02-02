e797a5651f609b9f079768169746dfb6b7d41d8c
AristideExe <aristidepr@gmail.com>
2026-02-02 13:57:54 +0100

    feat: add contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,12 +2,19 @@ import GreenButton from "../GreenButton/GreenButton.jsx";
 import {Link} from "react-router";
 
 const Contact = () => (
-    <div>
-        <Link to="mailto:ecologiie@iiens.net" target="_blank">
-            <GreenButton>
-                <p className="uppercase">Prendre contact</p>
-            </GreenButton>
-        </Link>
+    <div className="px-4 md:w-1/2 py-24 mx-auto">
+        <h1 className="uppercase font-extrabold text-3xl border-b border-black py-4">Prendre contact</h1>
+        <div className="flex py-8 gap-8 align-middle flex-col md:flex-row">
+            <p className="flex-1/2">tellus consectetur nibh dolor dolor, diam, Donec suscipit adipiscing at sollicitudin tincidunt laoreet dolor, auctor dolor, et tristique. Donec massa et mauris </p>
+            <div className="flex-1/2 flex flex-col gap-4">
+                <p>1 place de la resistance, 9100 Évry-Courcouronnes</p>
+                <Link to="mailto:ecologiie@iiens.net" target="_blank">
+                    <GreenButton>
+                        <p className="uppercase">Nous contacter</p>
+                    </GreenButton>
+                </Link>
+            </div>
+        </div>
     </div>
 );
 
--- src/components/GreenButton/GreenButton.jsx
+++ src/components/GreenButton/GreenButton.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 const GreenButton = ({ children }) => (
-    <button className="relative px-12 py-3 bg-white border border-green-600 text-green-600 font-medium overflow-hidden transition-colors duration-300 hover:text-white group cursor-pointer font-extrabold">
+    <button className="relative px-12 py-3 bg-white border border-green-600 text-green-600 overflow-hidden transition-colors duration-300 hover:text-white group cursor-pointer font-extrabold">
         <span className="relative z-10">{children}</span>
         <span className="absolute bottom-0 left-0 w-full h-0 bg-green-600 transition-all duration-500 ease-out group-hover:h-full"></span>
     </button>
